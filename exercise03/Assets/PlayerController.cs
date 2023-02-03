c5c0bf9001f2dfd66b05861b731180ab46913536
Radha-cal <radha.hosad@gmail.com>
2023-02-03 17:58:05 -0500

    game update

--- exercise03/Assets/PlayerController.cs
+++ exercise03/Assets/PlayerController.cs
@@ -1,9 +1,12 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float moveSpeed = 5f;
+    float rotateSpeed = 50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       float hAxis = Input.GetAxis("Horizontal");
+       float vAxis = Input.GetAxis("Vertical"); 
+       gameObject.transform.Translate(gameObject.transform.forward*Time.deltaTime * moveSpeed* vAxis, Space.World);
+       gameObject.transform.Rotate(0, rotateSpeed* Time.deltaTime *hAxis, 0);
     }
+    private void OnTriggerEnter(Collider other){
+         if (other.CompareTag("Collectables")){
+            Destroy(other.gameObject);
+        }
+    }
+
 }
